7cad53155f72d8fbcb960e2073845d538239d7bd
Drachir <gaming.drachir@gmail.com>
2024-04-26 17:19:23 +0200

    i forgor to add these :skull_emoji:

---
+++ cogs2/manage_commands.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import os
+
+class ManageCommands(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    async def reload(self, ctx):
+        if ctx.author.name == "drachir_":
+            new_exts = []
+            for e in os.listdir("cogs2"):
+                if "__pycache__" in e: continue
+                elif "cog_template" in e: continue
+                new_exts.append("cogs." + e.split(".")[0])
+            for extension in new_exts:
+                await self.client.reload_extension(extension)
+            await self.client.tree.sync(guild=None)
+            print("Reloaded: "+",".join(new_exts))
+        else:
+            ctx.channel.send("No permission to use this command.")
+    
+    @commands.command()
+    async def sync(self, ctx):
+        if ctx.author.name == "drachir_":
+            print(await self.client.tree.sync(guild=None))
+        else:
+            ctx.channel.send("No permission to use this command.")
+        
+async def setup(bot:commands.Bot):
+    await bot.add_cog(ManageCommands(bot))
